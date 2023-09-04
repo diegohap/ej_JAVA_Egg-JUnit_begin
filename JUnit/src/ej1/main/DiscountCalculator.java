bfc3c7e35ab693da87f11d45b45e5ed6e4f2a4a3
diegohap <diegohap@gmail.com>
2023-09-04 20:53:04 -0300

    :second commit

--- JUnit/src/ej1/main/DiscountCalculator.java
+++ JUnit/src/ej1/main/DiscountCalculator.java
@@ -1,10 +1,13 @@
 package ej1.main;
 
+import java.util.ArrayList;
+
 public class DiscountCalculator {
     public static final Double DESCUENTO_MAXIMO = 50.0;
     //Devuelve el % aplicado
     public Double calcularDescuent(Double precioConRebaja, Double precioOriginal) {
         Double descuento = (1-precioConRebaja/precioOriginal)*100;
+        ArrayList<Integer> l = new ArrayList<Integer>();
         return descuento <= DESCUENTO_MAXIMO ? descuento : precioOriginal*DESCUENTO_MAXIMO/100;
     }
 }
